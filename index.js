f1385919b93c90df67c022e6228a3512752e991c
Sai Peddi <saiprabhatpeddi@gmail.com>
2025-12-12 04:17:55 -0500

    half way through a6

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import session from "express-session";
 import express from "express";
+import mongoose from "mongoose";
 import Lab5 from "./Lab5/index.js";
 import db from "./Kambaz/Database/index.js";
 import UserRoutes from "./Kambaz/Users/routes.js";
@@ -8,6 +9,8 @@ import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
 import cors from "cors";
+const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
